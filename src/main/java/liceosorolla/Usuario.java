39f96014c09f7a1bffc7a8606ee3f4545fa19cb0
DAM_1 <DAM_1@PB09>
2020-02-12 17:57:51 +0100

    base develop

--- src/main/java/liceosorolla/Usuario.java
+++ src/main/java/liceosorolla/Usuario.java
@@ -1,5 +1,48 @@
 package main.java.liceosorolla;
 
 public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private String fecha;
+	private int identificador;
+	
+	public Usuario(String nombre, String apellidos, String fecha, int identificador) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fecha = fecha;
+		this.identificador = identificador;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+	
 }
--- src/test/java/liceosorolla/UsuarioTest.java
+++ src/test/java/liceosorolla/UsuarioTest.java
@@ -1,5 +1,17 @@
 package test.java.liceosorolla;
 
-public class UsuarioTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorolla.Usuario;
+
+public class UsuarioTest {
+	private Usuario usuario;
+	@Before
+	public void Before() {
+		usuario = new Usuario("Mario","Serrano Moreo", "2020-02-12", 7);
+	}
+	
 }
