1aff18de1de41527ee23d6098e0b961241f8bf98
DAM_1 <DAM_1@PB07>
2020-03-06 20:08:35 +0100

    rectangulo

--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,24 @@
 package test.java.liceosorolla;
 
-public class RectanguloTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Rectangulo;
+
+public class RectanguloTest {
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void Before() {
+		rectangulo = new Rectangulo(6,9);
+	}
+	
+	@Test
+	public void comprobarLados() {
+		boolean comprobar=false;
+		assertEquals(comprobar,rectangulo.esCuadrado());
+	}
+	
+	
 }
