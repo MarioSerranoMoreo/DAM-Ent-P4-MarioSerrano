654e2afaf111809bb02825267a19bad373f5d969
DAM_1 <DAM_1@PB10>
2020-03-06 19:25:24 +0100

    Mejora Animal

--- src/main/java/liceosorolla/Animal.java
+++ src/main/java/liceosorolla/Animal.java
@@ -1,5 +1,48 @@
 package main.java.liceosorolla;
 
 public class Animal {
-
+	private String animal;
+	private String especie;
+	private int peso;
+	private int edad;
+	private String colorPelo;
+	
+		public Animal(String animal, String especie, int peso, int edad, String colorPelo) {
+		this.animal = animal;
+		this.especie = especie;
+		this.peso = peso;
+		this.edad = edad;
+		this.colorPelo = colorPelo;
+	}
+	
+	public String getAnimal() {
+		return animal;
+	}
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getColorPelo() {
+		return colorPelo;
+	}
+	public void setColorPelo(String colorPelo) {
+		this.colorPelo = colorPelo;
+	}
 }
--- src/test/java/liceosorolla/AnimalTest.java
+++ src/test/java/liceosorolla/AnimalTest.java
@@ -1,5 +1,16 @@
 package test.java.liceosorolla;
 
-public class AnimalTest {
+import java.time.LocalDate;
 
+import org.junit.Before;
+
+import main.java.liceosorolla.Animal;
+import main.java.liceosorolla.Usuario;
+
+public class AnimalTest {
+	private Animal animal;
+	@Before
+	public void Before() {
+		animal = new Animal ("Rana","Cefalopodo", 100 , 19,"rosa");
+	}
 }
